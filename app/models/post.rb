49daab1c7fe73a61461f25ac0e569e174d78a39f
Kzm-s <amuzak.ikasas@gmail.com>
2022-08-30 16:48:22 +0900

    タグづけ機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,6 @@ class Post < ApplicationRecord
 
     belongs_to :user
     has_many :comments, dependent: :destroy
-    has_many :post_tags, through: :post_tag, dependent: :destroy
+    has_many :post_tags, dependent: :destroy
+    has_many :tags, through: :post_tags, dependent: :destroy
 end
