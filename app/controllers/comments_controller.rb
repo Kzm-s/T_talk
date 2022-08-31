e32dd31e390d12ba51f46e0e7ea0f4403cd97605
Kzm-s <amuzak.ikasas@gmail.com>
2022-08-31 19:43:31 +0900

    コメント機能 修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,18 @@
 class CommentsController < ApplicationController
-
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      redirect_to post_path(@post), notice: "コメントしました"
+    else
+      flash[:danger] = "投稿に失敗しました"
+      render post_path(@post)
+    end
+  end
     def edit
         @post = Post.find(params[:post_id])
         @comment = Comment.find(params[:id])
     end
-
     
     def update
       @post = Post.find(params[:post_id])
@@ -16,7 +24,6 @@ class CommentsController < ApplicationController
         render 'edit'
       end
     end
-
     def destroy
         @comment = Comment.find(params[:id])
         @comment.destroy
@@ -26,6 +33,6 @@ class CommentsController < ApplicationController
     
       private
       def comment_params
-        params.require(:comment).permit(:comment_content, :status).merge(post_id: params[:post_id])
+        params.require(:comment).permit(:comment_content, :status).merge(user_id: current_user.id, post_id: params[:post_id])
       end
 end
