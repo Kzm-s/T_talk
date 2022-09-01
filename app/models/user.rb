df958767e62eafc7cd4138548d604755039dd59a
Kzm-s <amuzak.ikasas@gmail.com>
2022-09-01 20:49:41 +0900

    フォロー機能導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,26 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :user_positions, dependent: :destroy
   has_many :positions, through: :user_positions, dependent: :destroy
+  # フォローをした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id", dependent: :destroy
+
+  # 一覧画面で使う
+  has_many :followings, through: :relationships, source: :follow
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+
+  def follow(user_id)
+    relationships.create(follow_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(follow_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
+
 end
