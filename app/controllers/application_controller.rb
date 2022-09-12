c932eb0db25421647f138f9dbb80b764cb6ebc25
Kzm-s <amuzak.ikasas@gmail.com>
2022-09-12 17:04:43 +0900

    adminカラムの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birth, :gender, :affiliation, :job_title, :career, :responsible, :subject, :grade, :other, position_ids: []])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :birth, :gender, :affiliation, :job_title, :career, :responsible, :subject, :grade, :other, :admin, position_ids: []])
   end
 end
